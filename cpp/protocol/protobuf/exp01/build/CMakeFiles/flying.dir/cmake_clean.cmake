2616fc20cb8e092cf3b826641e0ef30f5797db29
HBrightUp <hbrightup@gmail.com>
2025-08-25 20:43:31 +0800

    add golang

---
+++ cpp/protocol/protobuf/exp01/build/CMakeFiles/flying.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "/home/hml/project/lang-learn/cpp/protocol/protobuf/exp01/proto/person.pb.cc"
+  "/home/hml/project/lang-learn/cpp/protocol/protobuf/exp01/proto/person.pb.h"
+  "/home/hml/project/lang-learn/cpp/protocol/protobuf/exp01/proto/tree.pb.cc"
+  "/home/hml/project/lang-learn/cpp/protocol/protobuf/exp01/proto/tree.pb.h"
+  "CMakeFiles/flying.dir/main.cpp.o"
+  "CMakeFiles/flying.dir/main.cpp.o.d"
+  "CMakeFiles/flying.dir/proto/person.pb.cc.o"
+  "CMakeFiles/flying.dir/proto/person.pb.cc.o.d"
+  "CMakeFiles/flying.dir/proto/tree.pb.cc.o"
+  "CMakeFiles/flying.dir/proto/tree.pb.cc.o.d"
+  "flying"
+  "flying.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/flying.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
